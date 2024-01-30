3a1ee860455b1c5b80b50c4b92fe17143220be7f
matéo! <6matbub@gmail.com>
2024-01-29 18:21:39 -0800

    feat: add nvim-tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,12 +35,75 @@ local lsp_servers = {
 	"lua_ls"
 }
 
+-- mason and mason-lspconfig
 local function configure_mason()
 	require("mason").setup()
 	require("mason-lspconfig").setup {
-		ensure_installed = servers
+		ensure_installed = lsp_servers
 	}
 end
+
+-- nvim-treesitter
+local function configure_treesitter()
+	require('nvim-treesitter.configs').setup {
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm"
+			}
+		},
+		indent = {
+			enabled = true
+		},
+		highlight = {
+			enabled = true
+		},
+		autotag = {
+		 	enabled = true
+		},
+		textbjects = {
+			select = {
+				enabled = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				}
+			},
+			move = {
+				enabled = true,
+				set_jumps = true,
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer"
+				}
+			}
+		},
+		ensure_installed = "all"
+	}
+end
+
+local function configure_nvim_tree()
+	require('nvim-tree').setup {}
+end
+
 
 -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
 require('lazy').setup({
@@ -48,11 +111,22 @@ require('lazy').setup({
 		'neovim/nvim-lspconfig',
 		dependencies = {
 			{
-				'williamboman/mason.nvim', 
+				'williamboman/mason.nvim',
 				config = configure_mason
 			},
 			'williamboman/mason-lspconfig.nvim',
-			'folke/neodev.nvim'
+			'folke/neodev.nvim',
+			{
+				'nvim-treesitter/nvim-treesitter',
+				run = ':TSUpdate',
+				config = configure_treesitter
+			},
+			'windwp/nvim-ts-autotag',
+			{
+				'kyazdani42/nvim-tree.lua',
+				requires = 'kyazdani42/nvim-web-devicons',
+				config = configure_nvim_tree
+			}
 		},
 	}
 })
--- lua/settings.lua
+++ lua/settings.lua
@@ -6,4 +6,9 @@ vim.opt.number = true
 -- Show relative line numbers
 vim.opt.relativenumber = true
 
+-- Disable netrw 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
+-- Enable 24-bit color
+vim.opt.termguicolors = true
